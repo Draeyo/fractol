f4d532f4d99d38292f84887d5a9066df14382fcd
Victor LISTRAT <vlistrat@e2r11p3.42.fr>
2016-11-16 15:37:39 +0100

    end

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: vlistrat <vlistrat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 14:31:33 by vlistrat          #+#    #+#             */
-/*   Updated: 2016/11/16 11:09:25 by vlistrat         ###   ########.fr       */
+/*   Updated: 2016/11/16 15:37:31 by vlistrat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int		ft_exit(void *param)
 	ft_printf("\033[31mClosing ...\033[0m\n");
 	mlx_clear_window(MLX, WIN);
 	mlx_destroy_window(MLX, WIN);
-	system("killall afplay");
+	if (MUSIC)
+		system("killall afplay");
 	exit(EXIT_SUCCESS);
 	return (0);
 }
